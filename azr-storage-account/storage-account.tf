caf45e8418f3edb9deed767181652559705ddd2f
Guilherme Felipe <fellipeguilherme37@gmail.com>
2025-03-25 07:17:45 -0300

    configurando storage account na azure

---
+++ azr-storage-account/storage-account.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "resource-group" {
+  name     = "group-do-gui-dev"
+  location = "Brazil South"
+}
+
+resource "azurerm_storage_account" "storage-account" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "vhds"
+  storage_account_id    = azurerm_storage_account.example.id
+}
+
