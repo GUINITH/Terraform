21570428e8d1e6bd637356cd12fb781bbbc1913b
Guilherme Felipe <fellipeguilherme37@gmail.com>
2025-03-28 07:41:54 -0300

    ativando o sensitive =True

--- azr-storage-blob/output.tf
+++ azr-storage-blob/output.tf
@@ -21,6 +21,7 @@ output "storage_container_name" {
 output "sa_primary_access_key" {
   value = azurerm_storage_account.sa.primary_access_key
   description = 'Chave de acesso da storage accaunt'
+  sensitive = true
 }
 
 
